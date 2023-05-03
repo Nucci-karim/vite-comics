3aeac837059395cc8805edcf0a3e8fa32eb6268c
Nucci-karim <nuccikarim@gmail.com>
2023-05-03 16:40:13 +0200

    inizio impostazione con primo test di partials

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <h1>ciao</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  @use './style/main.scss' as *;
+
+  h1{
+    color: $colore-rosso;
+  }
+
+</style>
