63631da142892bbd363beef561a2c639ea8c52fc
Nucci-karim <nuccikarim@gmail.com>
2023-05-03 16:44:39 +0200

    aggiunta assets

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,13 @@
 
 <template>
   <div>
-    <h1>ciao</h1>
+    
   </div>
 </template>
 
 <style lang="scss" scoped>
   @use './style/main.scss' as *;
 
-  h1{
-    color: $colore-rosso;
-  }
+  
 
 </style>
