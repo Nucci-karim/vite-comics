9534080245b67c73ccc1ae86b5f0209679a83202
Nucci-karim <nuccikarim@gmail.com>
2023-05-03 19:26:29 +0200

    footer finito

--- src/App.vue
+++ src/App.vue
@@ -62,7 +62,7 @@
 </script>
 
 <template>
-  <div>
+  <div id="tutto">
     <!-- header -->
     <header>
       <!-- navbar -->
@@ -117,13 +117,102 @@
     <!-- footer -->
     <footer>
       <!-- all links -->
-      <section>
+      <section id="all-links">
+        <div class="container">
+          <div>
+            <ul>
+              <h2>DC COMICS</h2>
+              <li>
+                <a href="/">Characters</a>
+              </li>
+              <li>
+                <a href="/">Comics</a>
+              </li>
+              <li>
+                <a href="/">Movies</a>
+              </li>
+              <li>
+                <a href="/">TV</a>
+              </li>
+              <li>
+                <a href="/">Games</a>
+              </li>
+              <li>
+                <a href="/">Videos</a>
+              </li>
+              <li>
+                <a href="/">News</a>
+              </li>
 
+              <h2>SHOP</h2>
+              <li>
+                <a href="/">Shop DC</a>
+              </li>
+              <li>
+                <a href="/">Shop DC Collectibles</a>
+              </li>
+            </ul>
+            <ul>
+              <h2>DC</h2>
+              <li>
+                <a href="/">Terms Of Use</a>
+              </li>
+              <li>
+                <a href="/">Privacy policy (new)</a>
+              </li>
+              
+            </ul>
+            <ul>
+              <h2>SITES</h2>
+              <li>
+                <a href="/">DC</a>
+              </li>
+              <li>
+                <a href="/">MAD Magazine</a>
+              </li>
+              <li>
+                <a href="/">DC Kids</a>
+              </li>
+              <li>
+                <a href="/">DC Universe</a>
+              </li>
+              <li>
+                <a href="/">DC Power Visa</a>
+              </li>
+            </ul>
+          </div>
+          <img src="./assets/img/dc-logo-bg.png" alt="logo-bg">
+        </div>
       </section>
 
       <!-- SIGN-UP -->
-      <section>
+      <section id="sign-up">
+        <div id="sign-up-c" class="container">
+          <div>
+            <button>SIGN-UP NOW!</button>
+          </div>
 
+          <div>
+            <ul>
+              <h2>FOLLOW US</h2>
+              <li>
+                <img src="./assets/img/footer-facebook.png" alt="">
+              </li>
+              <li>
+                <img src="./assets/img/footer-twitter.png" alt="">
+              </li>
+              <li>
+                <img src="./assets/img/footer-youtube.png" alt="">
+              </li>
+              <li>
+                <img src="./assets/img/footer-pinterest.png" alt="">
+              </li>
+              <li>
+                <img src="./assets/img/footer-periscope.png" alt="">
+              </li>
+            </ul>
+          </div>
+        </div>
       </section>
     </footer>
   </div>
@@ -136,6 +225,10 @@
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+  }
+
+  #tutto{
+    height: 100vh;
   }
 
   nav{
@@ -211,6 +304,65 @@
           }
           }
         }
+    }
+  }
+
+  // parte footer
+  #all-links{
+    background-image: url(./assets/img/footer-bg.jpg);
+    background-size: cover;
+    padding: 1rem 0;
+    .container{
+      display: flex;
+      justify-content: space-between;
+      img{
+        width: 300px;
+      }
+      div{
+        display: flex;
+        ul{
+        list-style: none;
+        color: white;
+        margin: 0 1rem;
+        li{
+          padding: 0.25rem 0;
+          font-size: small;
+          a{
+            color: gray;
+            text-decoration: none;
+          }
+          }
+        }
+      }
+    }
+  }
+
+  #sign-up{
+    background-color: rgb(48 48 48);
+    padding: 1rem 0;
+    #sign-up-c{
+      display: flex;
+      justify-content: space-between;
+      button{
+        background-color: rgb(48 48 48);
+        color: white;
+        padding: 0.5rem;
+        border: 2px solid $blue-DC;
+      }
+      ul{
+        display: flex;
+        align-items: center;
+        list-style: none;
+        color: $blue-DC;
+        h2{
+          margin: 0 1rem;
+        }
+        li{
+          display: flex;
+          align-items: center;
+          margin: 0 0.5rem;
+        }
+      }
     }
   }
 </style>
